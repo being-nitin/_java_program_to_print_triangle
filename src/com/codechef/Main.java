2da6c975145f45fa940f343205a0a237788df3fe
Nitin <nsinghboss02@gmail.com>
2021-11-29 20:46:19 +0530

    taken number of lines from the user

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -1,21 +1,23 @@
 package com.codechef;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	/*
+	              /*
 	*
 	* *
 	* * *
 	* * * *
 	* * * * *
-	 */
-    for(int i=0;i<5;i++){
+                   */
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+    for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
             System.out.print("* ");
         }
         System.out.println();
     }
-
     }
 }
